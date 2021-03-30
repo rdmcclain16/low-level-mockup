337abd1b565fa9284aae8738d6714fb1f10d4840
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-03-29 21:09:22 -0500

    added js file with function

---
+++ script.js
@@ -0,0 +1,7 @@
+function numOfCookies () {
+    cookies = window.prompt('How Many Cookies Can You Eat?')
+    if (cookies >= 10)
+    document.write('Wow that is alot!')
+    else
+    document.write('Not too bad!')
+}
