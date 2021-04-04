e468a1a9fc858630cf35ac0112ea68dd2b7903e7
rdmcclain16 <74519618+rdmcclain16@users.noreply.github.com>
2021-04-04 11:04:51 -0500

    pushing to new branch

---
+++ script.js
@@ -0,0 +1,7 @@
+function numOfCookies () {
+    cookies = window.prompt('How Many Cookies Can You Eat?')
+    if (cookies >= 5)
+    document.write('Wow, Cookie Monster!')
+    else
+    document.write('Not too bad!')
+}
